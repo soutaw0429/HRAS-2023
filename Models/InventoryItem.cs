049849a0672a774c510d936679c6b9acc56f14df
Hayk Arzumanyan <Hayk Arzumanyan@DESKTOP-RMQLAFU>
2023-03-18 13:39:49 -0500

    Committing the InventoryItem Class.

---
+++ Models/InventoryItem.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRAS_2023.Models
+{
+    public class InventoryItem
+    {
+        private string stockID;
+        private int quantity;
+        private string description;
+        private long price;
+        //From Spec doc feedback: "you need an allowance for "virtual" inventory". 
+        //What is that??
+
+        [Required]
+        public string StockID
+        {
+            get { return stockID; }
+            set { stockID = value; }
+        }
+
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity can not be negative!")]
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        [Required]
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        [Required]
+        [Range(0, long.MaxValue, ErrorMessage = "Price can not be negative!")]
+        public long Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+
+        public InventoryItem(string _stockID, int _quantity, string _description, long _price)
+        {
+            stockID = _stockID;
+            quantity = _quantity; ;
+            description = _description; 
+            price = _price;
+        }
+
+    }
+}
