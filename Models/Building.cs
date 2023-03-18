9065d6f88d8ac29aedd457c397d7e83247a83170
Hayk Arzumanyan <Hayk Arzumanyan@DESKTOP-RMQLAFU>
2023-03-18 14:17:34 -0500

    Creating the Building Class

---
+++ Models/Building.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRAS_2023.Models
+{
+    public class Building
+    {
+        private string name;
+
+        [Required]
+        public string Name 
+        { 
+            get { return name; } 
+            set { name = value; } 
+        }
+
+        public Building(string _name)
+        {
+            name = _name;
+        }
+    }
+}
