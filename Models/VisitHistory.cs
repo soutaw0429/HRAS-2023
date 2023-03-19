872353ded64a0095e8a0a13b01f8ed235249283e
Hayk Arzumanyan <Hayk Arzumanyan@DESKTOP-RMQLAFU>
2023-03-18 19:03:48 -0500

    Created VisitHistory class (partially yet)

---
+++ Models/VisitHistory.cs
@@ -0,0 +1,11 @@
+﻿namespace HRAS_2023.Models
+{
+    public class VisitHistory : Patient 
+    {
+        private DateTime checkInDateTime;
+        private DateTime checkOutDateTime;
+
+        //stopping here for now to merge into main so I have access to all the code...
+
+    }
+}
