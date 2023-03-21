0ada74ee8bcd09012ad4629c5a5e1b430d79f7b3
Kevin Riley <kayrock973@yahoo.com>
2023-03-21 17:15:12 -0500

    Achieved desired layout for Login and Warning pages. CSS code in HTML files temporarily. NOTE: I HAD TO COMMENT OUT "ViewHistory.cs" TO REMOVE ERRORS CREATED FROM FILES DELETED DURING CLASS.

--- Models/VisitHistory.cs
+++ Models/VisitHistory.cs
@@ -1,43 +1,43 @@
-﻿using HRAS.Models;
+﻿//using HRAS.Models;
 
-namespace HRAS_2023.Models
-{
-    public class VisitHistory : PatientModel
-    {
-        private DateTime checkInDateTime;
-        private DateTime checkOutDateTime;
-        private List<InventoryItem> usedItems;
-        private List<string> symptoms;
-        private string diagnose;
+//namespace HRAS_2023.Models
+//{
+//    public class VisitHistory : PatientModel
+//    {
+//        private DateTime checkInDateTime;
+//        private DateTime checkOutDateTime;
+//        private List<InventoryItem> usedItems;
+//        private List<string> symptoms;
+//        private string diagnose;
 
-        public DateTime CheckInDateTime
-        {
-            get { return checkInDateTime; }
-            set { checkInDateTime = value; }
-        }
+//        public DateTime CheckInDateTime
+//        {
+//            get { return checkInDateTime; }
+//            set { checkInDateTime = value; }
+//        }
 
-        public DateTime CheckOutDateTime
-        {
-            get { return checkOutDateTime; }
-            set { checkOutDateTime = value; }
-        }
+//        public DateTime CheckOutDateTime
+//        {
+//            get { return checkOutDateTime; }
+//            set { checkOutDateTime = value; }
+//        }
 
-        public List<InventoryItem> UsedItems
-        {
-            get { return usedItems; }
-            set { usedItems = value; }
-        }
+//        public List<InventoryItem> UsedItems
+//        {
+//            get { return usedItems; }
+//            set { usedItems = value; }
+//        }
 
-        public List<string> Symptoms
-        {
-            get { return symptoms; }
-            set { symptoms = value; }
-        }
+//        public List<string> Symptoms
+//        {
+//            get { return symptoms; }
+//            set { symptoms = value; }
+//        }
 
-        public string Diagnose
-        {
-            get { return diagnose; }
-            set { diagnose = value; }
-        }
-    }
-}
+//        public string Diagnose
+//        {
+//            get { return diagnose; }
+//            set { diagnose = value; }
+//        }
+//    }
+//}
