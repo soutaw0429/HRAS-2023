c4b414221402745f03b0587dd50327aab84043d6
Kevin Riley <kayrock973@yahoo.com>
2023-03-21 16:19:18 -0500

    Deleted files mentioned in class.

--- Models/AdminModel.cs
+++
@@ -1,8 +0,0 @@
-namespace HRAS.Models;
-
-using System.ComponentModel.DataAnnotations;
-
-public class AdminModel : StaffModel
-{
-    // This model is unecessary as the conversion from UML to MVC models is not direct.
-}
