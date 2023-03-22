d5b519263ffd0135e99914f00334e162fd094110
Josh Carlisle <josh@treacle.me.uk>
2023-03-22 10:01:12 -0500

    Modified model files to match new SQL files

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿using HRAS.Context;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using HRAS.Context;
+using HRAS.Interfaces;
+using HRAS.Repository;
+using HRAS.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ISecurityService, SecurityService>();
+builder.Services.AddScoped<IStaffRepository, StaffRepository>();
 
 // Add database connection
 // builder.Services.AddDbContext<HRASContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("HRASContext")));
