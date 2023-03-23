92a17203a75e34bcd6c621c3f7dae1b0fad1b81b
Sota Wakamatsu <soutaw0429@gmail.com>
2023-03-23 00:11:47 -0500

    fixed FK name of Patient table in Modifies table and changed referencing value of FK_Modifies_Staff to UserName

--- Database/Relationship_types/Modifies.sql
+++ Database/Relationship_types/Modifies.sql
@@ -1,20 +1,15 @@
 Create table Modifies
 (
-	staff_key varchar(150),
 	inventory_key char(5),
 	patient_key varchar(9),
-	staff_BirthDate_key Date, 
-	staff_FirstName_key varchar(50), 
-	staff_MiddleInitial_key char(1), 
-	staff_LastName_key varchar(50),
+	staff_key varchar(50),
 
 Constraint PK_Modifies
 	primary key (staff_key, inventory_key),
 
 Constraint FK_Modifies_Staff
-	foreign key(staff_BirthDate_key, staff_FirstName_key, staff_MiddleInitial_key, staff_LastName_key)
-	references Staff(BirthDate, FirstName, MiddleInitial,
-	LastName),
+	foreign key(staff_key)
+	references Staff(UserName),
 
 Constraint FK_Modifies_Inventory
 	foreign key(inventory_key)
