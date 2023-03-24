bfaf7b403e0b1bd7619a25b9437890bf85e9cf30
hxue <hxue@uwsuper.edu>
2023-03-24 17:11:20 -0500

    modify patient, checksin, checksout, diagnoses

--- Database/Entity_Types/Patient.sql
+++ Database/Entity_Types/Patient.sql
@@ -1,9 +1,14 @@
 CREATE TABLE Patient (
-    SSN VARCHAR(9) PRIMARY KEY,
+    SSN VARCHAR(9),
     Insurer VARCHAR(50),
     [State] CHAR(2),
     ZIP VARCHAR(10),
     City VARCHAR(50),
     StreetAddress VARCHAR(100),
+<<<<<<< Updated upstream
 	VisitHistory_ChecksInDateTime DATETIME
+=======
+	VisitHistroy_ChecksInDateTime DATETIME
+    CONSTRAINT PK_Patient PRIMARY KEY (SSN)
+>>>>>>> Stashed changes
 );
--- Database/Relationship_types/ChecksIn.sql
+++ Database/Relationship_types/ChecksIn.sql
@@ -1,8 +1,8 @@
 CREATE TABLE ChecksIn (
-	ChecksInDateTime DATETIME,
 	Patient_SSN VARCHAR(9),
+	CheckInDateTime DATETIME,
 	Staff_UserName VARCHAR(50),
-	CONSTRAINT PK_ChecksIn PRIMARY KEY (Patient_SSN, Staff_UserName),
-	CONSTRAINT FK_ChecksIn_Patient FOREIGN KEY (Patient_SSN) REFERENCES Patient(SSN),
+	CONSTRAINT PK_ChecksIn PRIMARY KEY (Patient_SSN, CheckInDateTime, Staff_UserName),
+	CONSTRAINT FK_ChecksIn_VisitHistory FOREIGN KEY (Patient_SSN, CheckInDateTime) REFERENCES VisitHistory(Patient_SSN, CheckInDateTime),
 	CONSTRAINT FK_ChecksIn_Staff FOREIGN KEY (Staff_UserName) REFERENCES Staff(UserName)
 );
