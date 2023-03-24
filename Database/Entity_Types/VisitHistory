99f59bf9f5c1d7bc436f927da34e856945528b6a
Sota Wakamatsu <soutaw0429@gmail.com>
2023-03-24 17:59:10 -0500

    Created VisitHistory entity type and modified StaysIn relationship type.

---
+++ Database/Entity_Types/VisitHistory
@@ -0,0 +1,11 @@
+Create Table VisitHistory
+(
+patient_SSN varchar(9),
+CheckInDateTime DATETIME,
+CheckOutDateTime DATETIME,
+Diagnosis varchar(75),
+
+Constraint PK_VisitHistory Primary Key (patient_SSN, CheckInDateTime),
+Constraint FK_VisitHistory_Patient Foreign Key (patient_SSN)
+	References Patient (SSN)
+)
--- Database/Relationship_types/StaysIn.sql
+++ Database/Relationship_types/StaysIn.sql
@@ -4,16 +4,16 @@ isAdmitted BIT,
 leftTheRoom DateTime,
 room_building_key varchar(30),
 room_key varchar(9),
-patient_key varchar(9),
+history_patient_SSN varchar(9), 
+history_CheckInDateTime DATETIME,
 
-
-Constraint PK_StaysIn primary key(room_building_key, room_key, patient_key),
+Constraint PK_StaysIn primary key(room_building_key, room_key, history_patient_SSN, history_CheckInDateTime),
 
 Constraint FK_StaysIn_Room
-	foreign key(room_building_key, room_key)
-	references Room(building_key, room_number),
+    foreign key(room_building_key, room_key)
+    references Room(building_key, room_number),
 
-Constraint FK_StaysIn_Patient
-	foreign key(patient_key)
-	references Patient(SSN)
+Constraint FK_StaysIn_VisitHistory
+    foreign key(history_patient_SSN, history_CheckInDateTime)
+    references VisitHistory(patient_SSN, CheckInDateTime)
 )
