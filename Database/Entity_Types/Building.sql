2de495a50ade1569c6e6f931ab41dda12bfa8ee0
Hayk Arzumanyan <Hayk Arzumanyan@DESKTOP-RMQLAFU>
2023-03-23 19:11:13 -0500

    Modified the Building.sql file.

--- Database/Entity_Types/Building.sql
+++ Database/Entity_Types/Building.sql
@@ -1,11 +1,11 @@
 Create table Building
 (
-building_name varchar(30),
-street_address1 varchar(35),
-street_address2 varchar(35),
-city varchar(25),
-state char(2),
-
-zip char(5),
-Constraint PK_Building primary key(building_name)
+	building_name varchar(30),
+	--We will not be getting Building Addresses in the data files, so commented out the following variables:
+	--street_address1 varchar(35),
+	--street_address2 varchar(35),
+	--city varchar(25),
+	--state char(2),
+	--zip char(5),
+	Constraint PK_Building primary key(building_name)
 )
