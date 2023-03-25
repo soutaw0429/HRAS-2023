20a21dcaa07d556c6d340df7606fc896edd7fb25
Sota Wakamatsu <soutaw0429@gmail.com>
2023-03-25 13:24:18 -0500

    Modified foreign key of StaysIn and Modifies table.

--- Database/Relationship_types/Modifies.sql
+++ Database/Relationship_types/Modifies.sql
@@ -2,14 +2,12 @@ Create table Modifies
 (
 	staff_key varchar(150),
 	inventory_key char(5),
-	patient_key varchar(9),
-	staff_BirthDate_key Date, 
-	staff_FirstName_key varchar(50), 
-	staff_MiddleInitial_key char(1), 
-	staff_LastName_key varchar(50),
+	VisitHistory_patient_SSN varchar(9), 
+	VisitHistory_CheckInDateTime DATETIME,
+	
 
 Constraint PK_Modifies
-	primary key (staff_key, inventory_key),
+	primary key (staff_key, inventory_key, VisitHistory_patient_SSN, VisitHistory_CheckInDateTime),
 
 CONSTRAINT FK_Modifies_Staff
 	FOREIGN KEY (staff_key)
@@ -20,8 +18,8 @@ Constraint FK_Modifies_Inventory
 	foreign key(inventory_key)
 	references Inventory(stock_id),
 
-Constraint FK_Modifies_Patient
-	foreign key(patient_key)
-	references Patient(SSN),
+Constraint FK_Modifies_VisitHistory
+	foreign key(VisitHistory_patient_SSN, VisitHistory_CheckInDateTime)
+	references Patient(patient_SSN, CheckInDateTime),
 
 )
--- Database/Relationship_types/StaysIn.sql
+++ Database/Relationship_types/StaysIn.sql
@@ -2,18 +2,18 @@ Create table StaysIn
 (
 isAdmitted BIT,
 leftTheRoom DateTime,
-room_building_key varchar(30),
-room_key varchar(9),
-history_patient_SSN varchar(9), 
-history_CheckInDateTime DATETIME,
+room_building_name varchar(30),
+room_number varchar(9),
+visitHistory_patientSSN varchar(9), 
+visitHistory_checkInDateTime DATETIME,
 
-Constraint PK_StaysIn primary key(room_building_key, room_key, history_patient_SSN, history_CheckInDateTime),
+Constraint PK_StaysIn primary key(room_building_name, room_number, history_patient_SSN, history_CheckInDateTime),
 
 Constraint FK_StaysIn_Room
-    foreign key(room_building_key, room_key)
-    references Room(building_key, room_number),
+    foreign key(room_building_name, room_number)
+    references Room(building_key, [number]),
 
 Constraint FK_StaysIn_VisitHistory
-    foreign key(history_patient_SSN, history_CheckInDateTime)
+    foreign key(visitHistory_patientSSN, visitHistory_checkInDateTime)
     references VisitHistory(patient_SSN, CheckInDateTime)
 )
