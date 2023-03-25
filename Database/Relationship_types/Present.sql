eb87f13969b2dba47a43595c5e45c5bdfd3dee28
Hayk Arzumanyan <Hayk Arzumanyan@DESKTOP-RMQLAFU>
2023-03-25 16:04:44 -0500

    Corrected a type in Present.sql. For the future, we need to change the name of this file to Presents.sql (the same way we did with the model files at the beginning of this phase).

--- Database/Relationship_types/Present.sql
+++ Database/Relationship_types/Present.sql
@@ -1,7 +1,7 @@
-CREATE TABLE Present (
+CREATE TABLE Presents (
 	Symptom_Name VARCHAR(50),
 	Patient_SSN VARCHAR(9),
-	CONSTRAINT PK_Present PRIMARY KEY (Patient_SSN, Symptom_Name),
-	CONSTRAINT FK_Present_Patient FOREIGN KEY (Patient_SSN) REFERENCES Patient(SSN),
-	CONSTRAINT FK_Present_Symptom FOREIGN KEY (Symptom_Name) REFERENCES Symptom([Name])
+	CONSTRAINT PK_Presents PRIMARY KEY (Patient_SSN, Symptom_Name),
+	CONSTRAINT FK_Presents_Patient FOREIGN KEY (Patient_SSN) REFERENCES Patient(SSN),
+	CONSTRAINT FK_Presents_Symptom FOREIGN KEY (Symptom_Name) REFERENCES Symptom([Name])
 )
