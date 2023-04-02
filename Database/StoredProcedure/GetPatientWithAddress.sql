49395ef981207de3d9cd9d5ccbe06f70ce3b84d2
hxue <hxue@uwsuper.edu>
2023-04-02 14:44:58 -0500

    modify code

--- Database/StoredProcedure/GetPatientWithAddress.sql
+++ Database/StoredProcedure/GetPatientWithAddress.sql
@@ -1,13 +1,11 @@
 CREATE PROCEDURE GetPatientWithAddress
+	@SSN varchar(9)
 AS
 BEGIN
 	SELECT
 		*
 	FROM Patient, Home
-	WHERE Patient.SSN = Home.Patient_Key
+	WHERE Patient.SSN = @SSN AND Patient.SSN = Home.Patient_Key
 	ORDER BY
 		Patient.LastName;
 END
-		
-
-EXEC GetPatientWithAddress
