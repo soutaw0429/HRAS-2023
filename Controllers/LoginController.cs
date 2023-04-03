06184ea68fe400a0828b98e48e364779d377bbf0
Josh Carlisle <josh@treacle.me.uk>
2023-04-02 21:45:05 -0500

    Merge from main into loginSecurity

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,54 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using HRAS.Models;
+
+namespace HRAS.Controllers;
+
+public class LoginController : Controller
+{
+    private readonly ILogger<AuthController> _logger;
+
+    public LoginController(ILogger<AuthController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult WarningPage()
+    {
+        return View();
+    }
+
+    public IActionResult Login(int? id)
+    {
+        return View();
+    }
+    public IActionResult ProcessLogin(Staff staffMember) 
+    {
+        if (staffMember.userName == "JohnDoe" && staffMember.password == "testLogin")
+        {
+            return View("LoginSuccessTest", staffMember);
+        }
+        else
+        {
+            return View("LoginFailureTest", staffMember);
+        }
+
+        // Bellow is example of what will be implemented after service is created. 
+        // StaffSecurity staff = new StaffSecurity();
+
+        // if (staff.IsValid(staffMember))
+        // {
+        //    return View("LoginSuccess", staffMember);
+        // }
+        // else
+        // {
+        //    return View("LoginFailure", staffMember);
+        // }
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
