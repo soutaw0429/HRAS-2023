0d5432b2ae0948d2042b589c3e1a606cc3689728
Josh Carlisle <josh@treacle.me.uk>
2023-04-05 14:24:10 -0500

    Added stored procedure method

--- Repository/StaffRepository.cs
+++ Repository/StaffRepository.cs
@@ -1,5 +1,6 @@
 namespace HRAS.Repository;
 
+using System.Data.SqlClient;
 using System.Linq;
 using HRAS.Context;
 using HRAS.Interfaces;
@@ -18,5 +19,8 @@ public class StaffRepository : IStaffRepository
     {
         return _context.Staff
             .SingleOrDefault(staff => staff.userName!.Equals(username));
+
+        // var userIdParam = new SqlParameter("@UserName", username);
+        // return Staff.FromSqlRaw("EXEC FindPasswordByStaffUsername @UserName", username).FirstOrDefault();
     }
 }
