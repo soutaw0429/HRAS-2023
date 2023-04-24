524a60f5bc35f8a2e817bcb624262cb494d9c4ea
haykarzumanyan2002 <haykarzumanyan2002@gmail.com>
2023-04-24 14:48:52 -0500

    Removed te checkInDateTIme column from the ChecksIn table based on the design doc feedback.

--- Database/Relationship_types/ChecksIn.sql
+++ Database/Relationship_types/ChecksIn.sql
@@ -1,6 +1,5 @@
 CREATE TABLE ChecksIn (
 	Patient_SSN VARCHAR(9),
-	CheckInDateTime DATETIME,
 	Staff_UserName VARCHAR(25),
 	CONSTRAINT PK_ChecksIn PRIMARY KEY (Patient_SSN, CheckInDateTime, Staff_UserName),
 	CONSTRAINT FK_ChecksIn_VisitHistory FOREIGN KEY (Patient_SSN, CheckInDateTime) REFERENCES VisitHistory(Patient_SSN, CheckInDateTime),
