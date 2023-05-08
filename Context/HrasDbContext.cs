578244a0e122c03080b2885a6e06d8987933aac4
unknown <josh@treacle.me.uk>
2023-05-07 22:02:07 -0500

    Added Search algorithm

---
+++ Context/HrasDbContext.cs
@@ -0,0 +1,22 @@
+namespace HRAS_2023.Context;
+
+using Microsoft.EntityFrameworkCore;
+using HRAS_2023.Models;
+using HRAS_2023.ViewModels;
+
+public class HrasDbContext : DbContext
+{
+    public HrasDbContext(DbContextOptions<HrasDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Patient> Patient { get; set; } = null!;
+
+    public DbSet<Home> Home { get; set; } = null!;
+
+    public DbSet<Room> Room { get; set; } = null!;
+
+    public DbSet<VisitHistory> VisitHistory { get; set; } = null!;
+
+    public DbSet<SearchViewModel> SearchViewModel { get; set; } = null!;
+}
---
+++ ViewModels/SearchViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace HRAS_2023.ViewModels;
+
+using HRAS_2023.Models;
+
+public class SearchViewModel
+{
+    public Patient? Patient { get; set; }
+    public Home? Home { get; set; }
+    public Room? Room { get; set; }
+    public VisitHistory? VisitHistory { get; set; }
+}
