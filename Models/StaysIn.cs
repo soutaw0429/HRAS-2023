9ed7f7b801ca2576db87f2f5d0c89a02a81c1c0d
unknown <josh@treacle.me.uk>
2023-05-08 21:34:31 -0500

    Extra commit to make for absence on 28/03/2023

---
+++ Models/StaysIn.cs
@@ -0,0 +1,35 @@
+﻿namespace HRAS_2023.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+public class StaysIn
+{
+    [Required]
+    [Key]
+    [StringLength(1)]
+    public char isAdmitted { get; set; }
+
+    [Required]
+    [DataType(DataType.DateTime)]
+    public DateTime leftRoomDateTime { get; set; }
+
+    [Required]
+    [Key]
+    [StringLength(30)]
+    public string? room_building_name { get; set; }
+
+    [Required]
+    [Key]
+    [StringLength(9)]
+    public string? room_number { get; set; }
+
+    [Required]
+    [Key]
+    [StringLength(9)]
+    public string? visitHistory_patient { get; set; }
+
+    [Required]
+    [Key]
+    [DataType(DataType.DateTime)]
+    public DateTime visitHistory_checkInDateTime { get; set; }
+}
