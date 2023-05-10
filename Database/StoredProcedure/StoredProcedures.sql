85a491c647875f5fa162f4cc9b2a51dbe1d109cf
hxue <hxue@uwsuper.edu>
2023-05-09 21:24:57 -0500

    find_symptoms_near_medians_hasParameter, this one is not fully working. this commit is a make up for absent on 5.8

--- Database/StoredProcedure/StoredProcedures.sql
+++ Database/StoredProcedure/StoredProcedures.sql
@@ -73,3 +73,48 @@ BEGIN
     HAVING ABS(CAST(COUNT(*) as float) - @median) <= 15
     ORDER BY ABS(CAST(COUNT(*) as float) - @median);
 END
+
+CREATE PROCEDURE find_symptoms_near_medians_hasParameter @s1_name varchar(25)
+AS
+BEGIN
+    -- Create temporary table to store the first set of results
+    CREATE TABLE #temp_results (
+        symptom varchar(25),
+        frequency int
+    )
+
+    -- Calculate the median frequency
+    DECLARE @total_count int, @median float
+    SELECT @total_count = COUNT(*) FROM Presents
+    SELECT @median = AVG(CAST(frequency as float))
+    FROM
+    (
+        SELECT COUNT(*) as frequency
+        FROM Presents
+        GROUP BY Symptom_Name
+    ) AS frequency_table
+    WHERE frequency_table.frequency <= (@total_count + 1) / 2
+    
+    -- Find 10 symptoms whose frequency is near the median
+    INSERT INTO #temp_results (symptom, frequency)
+    SELECT TOP 10 Symptom_Name, COUNT(*) as frequency
+    FROM Presents
+    INNER JOIN VisitHistory ON Presents.Patient_SSN = VisitHistory.Patient_SSN
+    INNER JOIN Symptom ON Symptom.NAME = Presents.Symptom_Name
+    GROUP BY Symptom_Name
+    HAVING ABS(CAST(COUNT(*) as float) - @median) <= 5
+    ORDER BY ABS(CAST(COUNT(*) as float) - @median)
+
+    -- Find 10 symptoms whose frequency is near the median and include s1_name
+    SELECT TOP 10 Symptom_Name, COUNT(*) as frequency
+    FROM Presents
+    INNER JOIN VisitHistory ON Presents.Patient_SSN = VisitHistory.Patient_SSN
+    INNER JOIN Symptom ON Symptom.NAME = Presents.Symptom_Name
+    WHERE (Symptom_Name = @s1_name OR Symptom_Name IN (SELECT symptom FROM #temp_results))
+    GROUP BY Symptom_Name
+    HAVING ABS(CAST(COUNT(*) as float) - @median) <= 5
+    ORDER BY ABS(CAST(COUNT(*) as float) - @median)
+
+    -- Drop the temporary table
+    DROP TABLE #temp_results
+END
