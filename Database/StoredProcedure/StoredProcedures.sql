0d40831a993c30552a56c69252c00bc8738aa66d
haykarzumanyan2002 <haykarzumanyan2002@gmail.com>
2023-05-09 23:57:19 -0500

    Commenting out this SP. We need this for search.

--- Database/StoredProcedure/StoredProcedures.sql
+++ Database/StoredProcedure/StoredProcedures.sql
@@ -1,15 +1,15 @@
---CREATE PROCEDURE GetPatientWithAddress
---	@SSN varchar(9)
---AS
---BEGIN
---	SELECT
---		*
---	FROM Patient, Home
---	WHERE Patient.SSN = @SSN AND Patient.SSN = Home.Patient_Key
---	ORDER BY
---		Patient.LastName;
---END
-
+CREATE PROCEDURE GetPatientWithAddress
+	@SSN varchar(9)
+AS
+BEGIN
+	SELECT
+		*
+	FROM Patient, Home
+	WHERE Patient.SSN = @SSN AND Patient.SSN = Home.Patient_Key
+	ORDER BY
+		Patient.LastName;
+END
+GO
 
 --Create Procedure FindVisitHistoryByPatientSSN
 --	@SSN nvarchar(9)
