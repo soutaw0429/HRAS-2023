072a2b9a53decd89575175045123c1772478c1c5
hxue <hxue@uwsuper.edu>
2023-05-11 20:39:13 -0500

    added new SP

--- Database/Entity_Types/Home.sql
+++ Database/Entity_Types/Home.sql
@@ -12,3 +12,61 @@ CREATE TABLE Home
 	CONSTRAINT FK_Home_Patient FOREIGN KEY (Patient_Key)
 		REFERENCES Patient (SSN)
 )
+
+--added at 5.11
+
+CREATE PROCEDURE GetPatientBySSN
+	@SSN varchar(9)
+AS
+BEGIN
+	SELECT
+		Patient.SSN AS PatientSSN, Patient.FirstName AS PatientFirstName, Patient.LastName AS PatientLastName, Patient.MiddleInitial AS PatientMiddleInitial
+	FROM Patient
+	WHERE Patient.SSN = @SSN
+END
+
+CREATE PROCEDURE GetPatientByFirstName
+	@FirstName varchar(25)
+AS
+BEGIN
+	SELECT
+		Patient.SSN AS PatientSSN, Patient.FirstName AS PatientFirstName, Patient.LastName AS PatientLastName, Patient.MiddleInitial AS PatientMiddleInitial
+	FROM Patient
+	WHERE Patient.FirstName = @FirstName
+END
+
+CREATE PROCEDURE GetPatientByLastName
+	@LastName varchar(50)
+AS
+BEGIN
+	SELECT
+		Patient.SSN AS PatientSSN, Patient.FirstName AS PatientFirstName, Patient.LastName AS PatientLastName, Patient.MiddleInitial AS PatientMiddleInitial
+	FROM Patient
+	WHERE Patient.LastName = @LastName
+END
+
+Create Procedure GetPatientsByRoomNumber
+	@room_number varchar(9)
+As
+Begin
+	Select Patient.SSN AS PatientSSN, Patient.FirstName AS PatientFirstName, Patient.LastName AS PatientLastName, Patient.MiddleInitial AS PatientMiddleInitial
+	FROM Patient
+	Inner Join StaysIn On StaysIn.visitHistory_patientSSN = Patient.SSN
+	WHERE StaysIn.room_number = @room_number
+End
+
+Create Procedure getPatientsSSNFromRoom
+As
+Begin
+	Select StaysIn.visitHistory_patientSSN
+	FROM StaysIn
+End
+
+Create Procedure getHomeAddressBySSN
+	@SSN varchar(9)
+As
+Begin
+	Select StreetAddress_Line_1, StreetAddress_Line_2
+	FROM Home
+	WHERE Home.Patient_Key = @SSN
+End
